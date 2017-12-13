9e7ec83432aca943e62b9b0fdc35cf72a9a53e32
luoxin <luoxin@10.180.65.167>
2017-12-13 07:38:16 -0500

    a

--- XMLExample/src/main/java/pkgLibrary/Book.java
+++ XMLExample/src/main/java/pkgLibrary/Book.java
@@ -1,10 +1,17 @@
 package pkgLibrary;
 
 import java.util.Date;
-
-import javax.xml.bind.annotation.XmlAttribute;
+import java.util.ArrayList; 
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAttribute;
+
+
 
 public class Book {
 
@@ -15,23 +22,123 @@ public class Book {
 	private double price;
 	private Date publish_date;
 	private String description;
+	private double cost;
 
-	public Book() {
+	public static Book GetBook(String ID) throws BookException {
 
-	}
+		Catalog C = ReadXMLFile();
 
-	public Book(String id, String author, String title, String genre, double price, Date publish_date, String description)
-	{
-		super();
-		this.id = id;
-		this.author = author;
-		this.title = title;
-		this.genre = genre;		
-		this.price = price;
-		this.publish_date = publish_date;
-		this.description = description;
+		ArrayList<Book> books = C.getBooks();
+
+		Book Book1 = null;
+
+		for (Book a : books) {
+			if (a.getId().equals(ID)) {
+				Book1 = a;
+				}
+		}
+		if (Book1 != null) {
+			return Book1;
+		}
+		else{
+			
+			throw new BookException("Book does not find!");}
+
 	}
 	
+	public Book(String id) {
+		id = id;
+		try {
+			author = GetBook(id).getAuthor();
+			title = GetBook(id).getTitle();
+			genre = GetBook(id).getGenre();
+			price = GetBook(id).getPrice();
+			publish_date = GetBook(id).getPublish_date();
+			description = GetBook(id).getDescription();
+			cost = price * .8;
+		} catch (BookException exc) {
+			System.out.println(exc.getMessage());
+		}
+
+	}
+
+	
+	private static void WriteXMLFile(Catalog C) {
+		try {
+
+			String basePath = new File("").getAbsolutePath();
+			basePath = basePath + "\\src\\main\\resources\\XMLFiles\\Books.xml";
+			File file = new File(basePath);
+
+			JAXBContext JAX = JAXBContext.newInstance(Catalog.class);
+			Marshaller MAR = JAX.createMarshaller();
+
+			MAR.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			MAR.marshal(C, file);
+		    MAR.marshal(C, System.out);
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+
+	public static void addBook(String catID, Book newBook) throws BookException {
+		Catalog C = ReadXMLFile();
+
+		ArrayList<Book> books = C.getBooks();
+
+		Book Book1 = null;
+
+		for (Book b : books) {
+
+			if (b.getId().equals(newBook.getId())) {
+
+				Book1 = b;
+
+			}
+		}
+		if (Book1 == null) {
+
+			ArrayList<Book> catBooks = C.getBooks();
+
+			catBooks.add(newBook);
+
+			C.setBooks(catBooks);
+			WriteXMLFile(C);
+		}
+		else{
+			
+			throw new BookException("Book is already inside!");}
+
+		
+
+
+		}
+
+	private static Catalog ReadXMLFile() {
+
+		Catalog C = null;
+
+		String basePath = new File("").getAbsolutePath();
+		basePath = basePath + "\\src\\main\\resources\\XMLFiles\\Books.xml";
+		File file = new File(basePath);
+
+		System.out.println(file.getAbsolutePath());
+		try {
+			JAXBContext JAX = JAXBContext.newInstance(Catalog.class);
+			Unmarshaller UNM = JAX.createUnmarshaller();
+			C = (Catalog) UNM.unmarshal(file);
+			System.out.println(C);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+
+		return C;
+
+	}
  
 
 	public String getId() {
@@ -96,7 +203,18 @@ public class Book {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
+	public Book(String id, String author, String title, String genre, double price, Date publish_date, String description)
+	{
+		super();
+		this.id = id;
+		this.author = author;
+		this.title = title;
+		this.genre = genre;		
+		this.price = price;
+		this.publish_date = publish_date;
+		this.description = description;
+	}
+	
 	
 	
 
